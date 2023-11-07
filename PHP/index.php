c20742734ac75674f5fb08577fe0d2d710ba401f
Hmkien <manhkien203@gmail.com>
2023-11-07 16:13:43 +0700

    slide 24

--- PHP/index.php
+++ PHP/index.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>slide 19</title>
-</head>
-<body>
-    <?php 
-    echo"HELLO MY FRIEND ,YOU'R THE BEST!!";
-    ?>
-    <h3> Hello world </h3>
-    <select>
-        <option value="1">1</option>
-        <option value="2">2</option>
-    </select>
-</body>
-</html>
+<?php
+$TK=1000.000;
+$Pay=500.000;
+echo"số tiền ban đầu có là: $TK <br>";
+echo"số tiền cần chuyền khoản: $Pay <br>";
+$lastTK=$TK + $Pay;
+echo"số tiền sau khi chuyển khoản là: $lastTK";
