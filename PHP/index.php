5c90dc66a7ac293531f2889918d0fead172e7d22
Hmkien <manhkien203@gmail.com>
2023-11-08 00:27:56 +0700

    slide 36

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,11 @@
-<?php
-$subject = " lập trình web";
-$name = "HUMG";
-$number1= 2;
-$number2=2;
-echo"Học " . $subject ."ở phòng 502 tại trường" . $name . "<br>";
-echo"Học $subject ở phòng 502 tại trường  $name <br>";
-echo"<h1>$name </h1>";
-echo $number1 + $number2;
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+ Welcome to <?php echo $_GET["username"] ;?><br>
+ your password <?php echo $_GET["password"] ;?> 
+</body>
+</html>
