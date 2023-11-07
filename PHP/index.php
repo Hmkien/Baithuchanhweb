1cf470dd43995389e4874d6c0055b38960704f12
Hmkien <manhkien203@gmail.com>
2023-11-07 16:02:37 +0700

    slide 19

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,18 @@
-<?php
-echo" My first PHP file";
-echo"<h3> Hello world</h3>";
-echo"<br>";
-echo"<select>";
-echo"<option>1</option>";
-echo"<option>2</option>";
-echo"</select>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>slide 19</title>
+</head>
+<body>
+    <?php 
+    echo"HELLO MY FRIEND ,YOU'R THE BEST!!";
+    ?>
+    <h3> Hello world </h3>
+    <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+    </select>
+</body>
+</html>
