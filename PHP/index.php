9a7fdebadd2ac4429a94f5e82f5ad6505788096d
Hmkien <manhkien203@gmail.com>
2023-11-10 10:27:24 +0700

    slide 48

--- PHP/index.php
+++ PHP/index.php
@@ -1,53 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-    <style>
-        .login {
-            margin: 0 auto;
-            border: 1px solid black;
-            width: max-content;
-            height: max-content;
-            margin-top: 20%;
-            border-radius: 2em;
-            background-color: antiquewhite;
-        }
-
-        .login input[type="text"] {
-            margin-top: 2em;
-        }
-
-        .login input[type="submit"] {
-            margin-left: 38%;
-            margin-bottom: 2em;
-        }
-    </style>
+    <title>Document</title>
 </head>
-
 <body>
-    <form action="../PHP/index.php" method="get">
-        <div class="login">
-            <label for="username"> Tên đăng nhập:</label>
-            <input type="text" name="username"><br>
-            <label for="password">Mật Khẩu</label>
-            <input type="password" name="password"><br>
-            <br><br>
-            <input type="submit" value="Đăng Nhập">
-            <br>
-        </div>
+  <?php 
+if(isset($_GET['username']) && isset($_GET['password'])){
+$a = (float)$_GET['username'];
+$b=(float)$_GET['password'];
+$tong=$a + $b;
+echo $tong ;}
 
-    </form>
-    <?php 
-    if(isset($_GET['username'])&& isset($_GET['password'])){
-        $name = $_GET['username'];
-        $pw=$_GET['password'];
-        echo"wellcome" . $name . "có mật khẩu là:". $pw;
-    }
-    ?>
-
+?>  
 </body>
+</html>
 
-</html
