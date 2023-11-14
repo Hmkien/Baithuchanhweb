f1c339e221e0bf4edd59e8481d3717c60200c731
Hmkien <manhkien203@gmail.com>
2023-11-14 21:56:05 +0700

    slide 23

--- PHP/index.php
+++ PHP/index.php
@@ -3,18 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>slide 21</title>
 </head>
 <body>
-  <?php
-$date=date('d');
-$month =date('m');
-if(($date== '24')&&($month== '12')){
-    echo'<h1>ĐÚNG</h1>';
-}else 
-echo"<h1> Sai</h1>";
+    <form action="index.php" method="post">
+        <h1>CV công ty</h1>
+        <label for="name">Họ tên</label><br>
+        <input type="text" name="name" placeholder="họ tên" required><br>
+        <input type="radio" name="sex" value="nam">Nam<br>
+        <input type="radio"name="sex" value="nữ">Nữ<br>
+        <label for="Chieucao">Chiều cao(m)</label><br>
+        <input type="text" name="Chieucao"><br>
+        <label for="Cannang">cân nặng(kg)</label><br>
+        <input type="text" name="Cannang"><br>
+        <input  type="submit" value="Kết Quả">
 
-?>  
+    </form>
+    <?php 
+    if(isset($_POST["Chieucao"]) && isset($_POST["Cannang"]) && isset($_POST["sex"]) && isset($_POST["name"])){
+        $Chieucao=(float)$_POST["Chieucao"];
+        $Cannang=(float)$_POST["Cannang"];
+        $sex= $_POST["sex"];
+        $name = $_POST["name"];
+        if(($sex=="nam")&&($Cannang>50)&&($Chieucao>1.5)){
+            echo"chào bạn" .$name ."Bạn đã trúng tuyển ";
+        }
+        elseif(($sex=="nữ")&&($Cannang>40)&&($Chieucao>1.5)&&($Cannang<70)){
+        echo"chào bạn" .$name ."Bạn đã trúng tuyển ";
+    }
+    else 
+    echo"bạn".$name ."không đạt các chỉ tiêu của chúng tôi";
+    }
+    ?>
 </body>
 </html>
-
