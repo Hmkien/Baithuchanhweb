b08763f6c3d229cefa460694ff3b41b5aa928985
Hmkien <manhkien203@gmail.com>
2023-11-14 15:46:52 +0700

    slide 17

--- PHP/index.php
+++ PHP/index.php
@@ -6,26 +6,40 @@
     <title>Document</title>
 </head>
 <body>
- <form action="index.php" method="post">
-    <h1>Quite The Game</h1>
-    <select name ="traloi">
-        <option value="Yes">Yes</option>
-        <option value="No">No</option>
+<form action="index.php" method="post">
+    <h1> Hoán đổi tiền tệ</h1>
+    <label for="first">nhập vào số tiền cần đổi</label><br>
+    <input type="text" name="firt" required>
+    <label>Loại tiền:</label><br>
+    <select name="name">
+        <option value="USD">USD</option>
+        <option value="AUD">AUD</option>
+        <option value="JPY">JPY</option>
+        <option value="EUR">EUR</option>
     </select>
-    <input type="submit" value="submit">
- </form>
- <?php 
- if(isset($_POST["traloi"])) {
- echo" THe Game";
- if($_POST['traloi']=='Yes') {
-    echo" Thoát Game";
- }
- else
- {
-    echo "Game sẽ tiếp tục sau 3s <br>";
- }
- echo"game dang tiếp tục";
-}
- ?>
+    <input type="submit" value="Chuyển đổi">
+</form>
+<?php 
+if(isset($_POST["firt"]) &&isset($_POST["name"])){
+    $tien=(float)$_POST["firt"] ;
+    $ten=$_POST["name"] ;
+    if($_POST["name"]== "USD"){
+        $kq=23000*$tien;
+        echo"Kết quả=$kq";
+        }
+    elseif($_POST["name"]== "AUD"){
+        $kq= 17000*$tien;
+        echo "Kết quả=$kq";
+    }
+    elseif($_POST["name"]== "JPY"){
+        $kq=200*$tien;
+        echo "Kết quả=$kq";
+    }
+    else{
+        $kq= 27000*$tien;
+        echo"Kết quả=$kq";
+    }
+    }
+?>
 </body>
 </html>
