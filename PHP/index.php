1d70c6eee45a556e3e6af3f3ce45f1ee18b7560d
Hmkien <manhkien203@gmail.com>
2023-11-14 15:14:22 +0700

    slide 3

--- PHP/index.php
+++ PHP/index.php
@@ -1,25 +1,6 @@
 <?php
-// phương thức Get
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    if(isset($_GET["hoTen"]) && isset($_GET["PW"]) && isset($_GET["email"]) && isset($_GET["Sdt"])){
-        $hoTen = $_GET["hoTen"];
-        $PW = $_GET['PW'];
-        $email = $_GET['email'];
-        $Sdt = $_GET['Sdt'];
-        echo "Xin chào " . $hoTen . ", bạn vừa nhập mật khẩu là " . $PW . ", email: " . $email . " và số điện thoại: " . $Sdt . " (phương thức GET)";
-    }
+$items = array("about-1.jpg", "about-2.jpg", "about.jpg");
+foreach ($items as $item) {
+  echo "<img src='images/$item' width='100px' height='70px' alt='$item' />";
 }
-
-// phương thức post
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(isset($_POST["hoTen"]) && isset($_POST["PW"]) && isset($_POST["email"]) && isset($_POST["Sdt"])){
-        $hoTen = $_POST["hoTen"];
-        $PW = $_POST['PW'];
-        $email = $_POST['email'];
-        $Sdt = $_POST['Sdt'];
-        echo "Xin chào " . $hoTen . ", bạn vừa nhập mật khẩu là " . $PW . ", email: " . $email . " và số điện thoại: " . $Sdt . " (phương thức POST)";
-    }
-}
-
-
 ?>
