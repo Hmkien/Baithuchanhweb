d6c430dba18956d4661b41c5b80af85ebe3f99aa
Hmkien <manhkien203@gmail.com>
2023-11-14 22:30:27 +0700

    slide 24

--- PHP/index.php
+++ PHP/index.php
@@ -3,36 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>slide 21</title>
+    <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="post">
-        <h1>CV công ty</h1>
-        <label for="name">Họ tên</label><br>
-        <input type="text" name="name" placeholder="họ tên" required><br>
-        <input type="radio" name="sex" value="nam">Nam<br>
-        <input type="radio"name="sex" value="nữ">Nữ<br>
-        <label for="Chieucao">Chiều cao(m)</label><br>
-        <input type="text" name="Chieucao"><br>
-        <label for="Cannang">cân nặng(kg)</label><br>
-        <input type="text" name="Cannang"><br>
-        <input  type="submit" value="Kết Quả">
-
+    <form method="post" action="index.php">
+        <h1>Hối đoán tiền tệ:</h1><br>
+        <label for="money">Số tiền muốn đổi:</label><br>
+        <input type="text" name="money" required><br>
+        <select name="type">
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="SGD">SGD</option>
+            <option value="JPY">JPY</option>
+        </select>
+        <br>
+        <input type="submit" value="Chuyển đổi" >
     </form>
-    <?php 
-    if(isset($_POST["Chieucao"]) && isset($_POST["Cannang"]) && isset($_POST["sex"]) && isset($_POST["name"])){
-        $Chieucao=(float)$_POST["Chieucao"];
-        $Cannang=(float)$_POST["Cannang"];
-        $sex= $_POST["sex"];
-        $name = $_POST["name"];
-        if(($sex=="nam")&&($Cannang>50)&&($Chieucao>1.5)){
-            echo"chào bạn" .$name ."Bạn đã trúng tuyển ";
+    <?php
+    if(isset($_POST['money'])&& isset($_POST['type'])){
+        $type=$_POST['type'];
+        $first=(float)$_POST['money'];
+
+        $usd=23000;
+        $jpy=200;
+        $eur=27000;
+        $sgd=10000;
+        switch($type){
+            case 'USD': echo $usd *$first;
+            break;
+            case 'EUR': echo $first * $eur;
+            break;
+            case 'SGD': echo $first * $sgd;
+            break;
+            case 'JPY': echo $first * $jpy;
+
         }
-        elseif(($sex=="nữ")&&($Cannang>40)&&($Chieucao>1.5)&&($Cannang<70)){
-        echo"chào bạn" .$name ."Bạn đã trúng tuyển ";
-    }
-    else 
-    echo"bạn".$name ."không đạt các chỉ tiêu của chúng tôi";
+
     }
     ?>
 </body>
