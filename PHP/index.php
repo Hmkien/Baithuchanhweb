b43f08ece4af9c7a39266f50138ac3737e230b9b
Hmkien <manhkien203@gmail.com>
2023-11-14 15:22:01 +0700

    slide 3

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,24 @@
-<?php
-$items = array("about-1.jpg", "about-2.jpg", "about.jpg");
-foreach ($items as $item) {
-  echo "<img src='images/$item' width='100px' height='70px' alt='$item' />";
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $T= true;
+    $F= false;
+    echo"T= $T <br>";
+    echo"F= $F <br>";
+    $check1= 9>3;
+    $check2 = 10<4;
+    $check3 = $check1 || $check2;
+    $check4= $check1 && $check2;
+    echo " Check1 =$check1 <br>";
+    echo " Check2 =$check2 <br>";
+    echo " Check3 =$check3 <br>";
+    echo " Check4 =$check4 ";
+    ?>
+</body>
+</html>
