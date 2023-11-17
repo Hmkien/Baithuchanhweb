50cf1796710314d5d20fd3ce4bf2c73346190529
Hmkien <manhkien203@gmail.com>
2023-11-17 09:59:56 +0700

    slide 28

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,14 @@
 <?php
 $giatien=array(23000,27000,17000,120);
+$giatien[]=16000;
+$giatien[]= 18000;
+$giatien[0]= 25000;
 echo"Hoán đối tiền tệ <br>";
 echo" USD:" . $giatien[0] . "<br>" ;
 echo "EUR". $giatien[1]."<br>" ;
 echo "SGP". $giatien[2] ."<br>";
-echo "JPY". $giatien[3] ;
+echo "JPY". $giatien[3] ."<br>";
+echo "CHIN". $giatien[4] ."<br>";
+echo "AUD". $giatien[5] ."<br>";
 
 ?>
