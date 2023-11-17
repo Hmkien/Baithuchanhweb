37a6312759a39d81f3893068e8423251dfcb8c7f
Hmkien <manhkien203@gmail.com>
2023-11-17 10:13:59 +0700

    sldie 32

--- PHP/index.php
+++ PHP/index.php
@@ -1,11 +1,27 @@
-<?php
-$giatien=array("USD"=>23000,"EUR"=>27000,"SGP"=>17000,"JPY"=>120);
-
-echo"<h3>Hoán đối tiền tệ </h3><br>";
-echo" USD:" . $giatien['USD'] . "<br>" ;
-echo "EUR". $giatien['EUR']."<br>" ;
-echo "SGP". $giatien['SGP'] ."<br>";
-echo "JPY". $giatien['JPY'] ."<br>";
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hà Mạnh Kiên</title>
+</head>
+<body>
+    <h1> Hoán đối tiền tệ</h1>
+    <form method="post" action="index.php">
+        <label for="money">Nhập số tiền</label>
+        <input type="text"  name="money">
+        <label for="type-money"></label>
+        <input type="text" name="type-money">
+        <input type="submit" value="Hoán đối">
+    </form>
+    <?php 
+    if(isset($_POST['money'])&& isset($_POST['type-money'])) {
+        $giatien=array("USD"=>23000,"EUR"=>27000,"SGP"=>17000,"JPY"=>120);
+        $money=$_POST['money'];
+        $typemoney=strtoupper($_POST['type-money']);
+        echo"$money là bằng " . number_format($money * $giatien[$typemoney]) ."VND";
+        
+    }
+    ?>
+</body>
+</html>
