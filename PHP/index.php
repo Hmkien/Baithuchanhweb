99e5c7ba759291576b5710e48f92218f7ae81796
Hmkien <manhkien203@gmail.com>
2023-11-17 09:56:43 +0700

    slide 26

--- PHP/index.php
+++ PHP/index.php
@@ -1,45 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form method="post" action="index.php">
-        <h1>Hối đoán tiền tệ:</h1><br>
-        <label for="money">Số tiền muốn đổi:</label><br>
-        <input type="text" name="money" required><br>
-        <select name="type">
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-            <option value="SGD">SGD</option>
-            <option value="JPY">JPY</option>
-        </select>
-        <br>
-        <input type="submit" value="Chuyển đổi" >
-    </form>
-    <?php
-    if(isset($_POST['money'])&& isset($_POST['type'])){
-        $type=$_POST['type'];
-        $first=(float)$_POST['money'];
+<?php
+$giatien=array(23000,27000,17000,120);
+echo"Hoán đối tiền tệ <br>";
+echo" USD:" . $giatien[0] . "<br>" ;
+echo "EUR". $giatien[1]."<br>" ;
+echo "SGP". $giatien[2] ."<br>";
+echo "JPY". $giatien[3] ;
 
-        $usd=23000;
-        $jpy=200;
-        $eur=27000;
-        $sgd=10000;
-        switch($type){
-            case 'USD': echo $usd *$first;
-            break;
-            case 'EUR': echo $first * $eur;
-            break;
-            case 'SGD': echo $first * $sgd;
-            break;
-            case 'JPY': echo $first * $jpy;
-
-        }
-
-    }
-    ?>
-</body>
-</html>
+?>
