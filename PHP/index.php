8c255f121da39f73be404aba5a1dea0b5364c256
Hmkien <manhkien203@gmail.com>
2023-11-17 10:19:43 +0700

    slide 33

--- PHP/index.php
+++ PHP/index.php
@@ -16,10 +16,21 @@
     </form>
     <?php 
     if(isset($_POST['money'])&& isset($_POST['type-money'])) {
-        $giatien=array("USD"=>23000,"EUR"=>27000,"SGP"=>17000,"JPY"=>120);
+        $USD=23000;
+        $EUR=27000;
+        $SGP=17000;
+        $JPY=120;
         $money=$_POST['money'];
         $typemoney=strtoupper($_POST['type-money']);
-        echo"$money là bằng " . number_format($money * $giatien[$typemoney]) ."VND";
+       if($typemoney== 'USD')
+       echo $money * $USD  ;
+        elseif($typemoney=='EUR')
+        echo $money * $EUR ;
+    elseif($typemoney== 'SGP')
+    echo $money * $SGP ;
+        else
+        echo $money * $JPY ;
+
         
     }
     ?>
