033d1c1ac8123eaa5bfd55a0e59ea37a4ddc8976
Hmkien <manhkien203@gmail.com>
2023-11-24 10:01:33 +0700

    slide 7

--- PHP/index.php
+++ PHP/index.php
@@ -6,11 +6,10 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
-    $temps =array ('D:\PHP\htdocs\mkien\image\team-1.jpg','D:\PHP\htdocs\mkien\image\team-2.jpg','D:\PHP\htdocs\mkien\image\team-3.jpg','D:\PHP\htdocs\mkien\image\team-4.jpg');
-    foreach ($temps as $key) {
-        echo"<img src='images/$key' width='100px' height='100px' alt='$key'>";
-    }
-    ?>
+<?php
+for ($i = 1; $i < 10; $i++) {
+    echo"dãy số là $i <br>";
+}
+?>
 </body>
 </html>
