34ba38d17fd3692e5384341c189129ad55f5d06e
Hmkien <manhkien203@gmail.com>
2023-11-28 15:45:09 +0700

    slide 21

--- PHP/index.php
+++ PHP/index.php
@@ -3,23 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bài tập</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php 
-    $images=array('team-1.jpg','team-2.jpg','team-3.jpg','team-4.jpg');
-    foreach($images as $image){
-        echo "<img src='../image//$image' width='150px' height='150px' alt='$image'>";
+    <?php
+    function HienThi($List_image){
+        foreach ($List_image as $key) {
+            echo"<img src='../image/$key' width='150px' height='150px' alt='$key'>";
     }
+}
+$images=array('team-1.jpg','team-2.jpg','team-3.jpg','team-4.jpg');
+HienThi($images);
 
     ?>
-<h1>Function</h1>
-<?php
-    $image=array('team-1.jpg','team-2.jpg');
-    foreach($image as $i){
-        echo "<img src='../image//$i' width='150px' height='150px' alt='$i'>";
-    }
-
+    <h1>function</h1>
+    <?php
+    $images=array('team-1.jpg','team-2.jpg');
+    HienThi($images);
     ?>
 </body>
 </html>
