77b15314f4c287ca608581f17115a4a417540f13
Hmkien <manhkien203@gmail.com>
2023-12-01 21:17:46 +0700

    slide 55

--- PHP/index.php
+++ PHP/index.php
@@ -1,12 +1,40 @@
 <?php
 require('connect.php');
-$addsql="insert into products (id,name,quality,price) values('DU05','Pepsi','10','5000')";
-if($conn->query($addsql) === TRUE) {
-    echo "Thành công";
+mysqli_set_charset($conn,'UTF8');
+$sql="select * from products";
+$ketqua=$conn->query($sql);
+if($ketqua->num_rows > 0) {
+    echo"<table>
+    <tr>
+    <th>mã sản phẩm</th>
+    <th>tên sản phẩm</th>
+    <th>số lượng </th>
+    <th>giá </th>
+    </tr>"
+    ;
+    while($row = $ketqua->fetch_assoc()) {
+        echo"<tr>
+        <td>".$row["id"]."</td>
+        <td>".$row["name"]."</td>
+        <td>".$row["quality"]."</td>
+        <td>".$row["price"]."</td>
+        </tr>";
+    }
+    echo "<style>
+    table{
+        width:100%;
+        boder-collapse:collapse;
+    }
+    tr,th,td{
+        border:1px solid black;
+        boder-collapse:collapse;
+        text-align:center;
+
+    }
+    </style>";
 }
 else {
-echo"thêm ".$addsql."bị lỗi".$conn->error;
-    }
-
-$conn->close();
+    echo"không có dữ liệu";
+}
+$ketqua->close();
 ?>
