5e3dc3af06aefc4f8d2aa0c7a983f191d123bf02
Hmkien <manhkien203@gmail.com>
2023-12-05 16:17:12 +0700

    sldie 72a

--- PHP/index.php
+++ PHP/index.php
@@ -49,39 +49,48 @@
 <<div class="container">
     <form action="index.php" method="post">
         <h1 style="text-align: center;"> </h1>
-        <label>Họ và tên</label><br>
-        <input type="text" name="name"required><br>
         <label>origin</label><br>
         <input type="text" name="origin" required><br>
         <label>destination</label><br>
         <input type="text" name="destination" required><br>
-        <label> duration</label><br>
-        <input type="text" name="duration" required><br>
-        <input type="submit" name="submit" value="Đăng kí"><br>
+        <input type="submit" name="submit" value="Tìm kiếm"><br>
         
     </form>
 </div>
 
 <?php
-if (isset($_POST['origin']) && isset($_POST['destination'])&& isset($_POST['name'])&& isset($_POST['duration'])){
+if (isset($_POST['origin']) && isset($_POST['destination'])){
     $origin = $_POST['origin'];
     $destination= $_POST['destination'];
-    $name = $_POST['name'];
-    $duration = $_POST['duration'];
     require('connect.php');
     mysqli_set_charset($conn, 'UTF-8');
-    $sql = "INSERT  into passenger (origin, destination, duration,name ) values('$origin','$destination','$duration','$name')";
-    if($conn->query($sql)===true){
-        echo "đăng kí thành công";
-    } else {
-        echo "lỗi".$conn->error;
-    }
-    $conn->close();
+    $sql = "select * from passenger where origin='$origin' and destination='$destination'";
+    $ketqua =$conn->query($sql);
+    if($ketqua->num_rows>0){
+        echo "<table> 
+            <tr>
+                <th>id</th>
+                <th>origin</th>
+                <th>destination</th>
+                <th>duration</th>
+                <th>name</th>
+            </tr>";
+        if($row=$ketqua->fetch_assoc()){
+            echo "<tr>
+            <td>".$row["id"]."</td>
+            <td>".$row["origin"]."</td>
+            <td>".$row["destination"]."</td>
+            <td>".$row["duration"]."</td>
+            <td>".$row["Name"]."</td>
+          </tr>";
 }
-
-
+echo "</table>"; 
+} else {
+echo "không có dữ liệu";
+}
+$conn->close();
+}
 ?>
-
 
 </body>
 
