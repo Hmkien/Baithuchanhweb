68035772eba3681b9d6766b9f0fc2d4c00ff9d5f
Hmkien <manhkien203@gmail.com>
2023-12-07 21:22:06 +0700

    sldie 77

--- PHP/index.php
+++ PHP/index.php
@@ -46,18 +46,38 @@
     </style>
 </head>
 <body>
-    <?php
-    require('connect.php');
-    mysqli_set_charset ($conn, 'UTF-8');
-    $sql="DELETE FROM passenger WHERE id='14'";
-    if($conn->query($sql)==True){
-        echo"Xóa thành công";
-    }
-    else{
-        echo "lõi".$conn->error;
-    }
-    $conn->close();
+  <form method="post" action="index.php">
+        <lable>Name</lable><br>
+        <input type="text" name="Name" required>  <br>
+        <lable>Origin</lable> <br>
+        <input type="text"  name="Origin" required> <br>
+        <label>Destination</label> <br>
+        <input type="text" name="Destination" required> <br>
+        <input type="submit" value="Xóa" /> <br>
+  </form>
+  <?php
+  require('connect.php');
+  mysqli_set_charset($conn,'utf8');
+  if(isset($_POST['Name'])&&isset($_POST['Origin'])&&isset($_POST['Destination'])){
+    $name=$_POST['Name'];
+    $origin=$_POST['Origin'];
+    $destination=$_POST['Destination'];
+    $sql1="select * from passenger where name='$name' and origin='$origin' and destination='$destination'";
+    $result1=mysqli_query($conn,$sql1);
 
-    ?>
+    if($mysqli_num_rows> 0){
+  $sql="DELETE FROM passenger WHERE Name='$name' AND Origin='$origin' AND Destination='$destination";
+    if($conn->query($sql)){
+        echo"<script>alert('xóa thành công')</script>";
+  }
+  else{
+    echo "lõi".$conn->error;
+  }
+}
+else{
+    echo "không có dữ liệu";
+}
+}
+  ?>
 </body>
 </html>
