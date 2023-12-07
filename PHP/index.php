42b3a263a5320b45c4967211b56637ec280149b0
Hmkien <manhkien203@gmail.com>
2023-12-07 22:16:49 +0700

    slide 81

--- PHP/index.php
+++ PHP/index.php
@@ -47,53 +47,16 @@
 </head>
 <body>
 <?php
-echo "<form action='index.php' method='post'>";
-  require('connect.php');
-  mysqli_set_charset($conn,'utf8');
-  $sql = "select * from passenger";
-  $ketqua = $conn->query($sql);
-      if ($ketqua->num_rows > 0) {
-          echo "<table> 
-              <tr>
-              <td>option</td>
-                  <th>id</th>
-                  <th>origin</th>
-                  <th>destination</th>
-                  <th>duration</th>
-                  <th>Name</th>
-              </tr>";
-  
-          while ($row = $ketqua->fetch_assoc()) {
-              echo "<tr>
-              <td><input type='checkbox' name='checkbox[]' value='".$row['id']."'></td>
-                      <td>".$row["id"]."</td>
-                      <td>".$row["origin"]."</td>
-                      <td>".$row["destination"]."</td>
-                      <td>".$row["duration"]."</td>
-                    </tr>";
-          }
-          echo "</table>"; 
-      } else {
-          echo "không có dữ liệu";
-      }
-      echo"<input type='submit' name='submit'>";
-
-?>
-<?php
-        if(isset($_POST["submit"])){
-            if(isset($_POST["checkbox"])){
-                $abc=$_POST["checkbox"];
-                foreach( $abc as $id) {
-        $sql= "DELETE FROM passenger WHERE id=$id";
-       
-        }
-        if($conn->query($sql)===TRUE){
-            echo "<script>alert('XÓa thành công')</script>";
-    }
+require('connect.php');
+mysqli_set_charset($conn,'utf8');
+$sql="UPDATE products SET price='1000' WHERE id='DA02'";
+if($conn->query($sql)===TRUE){
+    echo "Thành công";
 }
-$conn->close();
-        }
-  ?>
+else {
+    echo "lỗi".mysqli_error($conn);
+}
+?>
 
 </body>
 </html>
